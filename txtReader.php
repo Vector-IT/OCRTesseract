f1e1137a70bb115273b7e40800b239b91a978cf3
Jose Maria Romero Cuny <jose.romero@vector-it.com.ar>
2024-08-29 08:20:17 -0300

    Mejora menor

---
+++ txtReader.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="por">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>txtReader</title>
+</head>
+<body>
+<?php
+ 
+require('class.pdf2text.php');
+extract($_POST);
+
+if(isset($readpdf)){
+    
+    if($_FILES['file']['type']=="application/pdf") {
+		$horaInicio = microtime(true);
+
+        $a = new PDF2Text();
+        $a->setFilename($_FILES['file']['tmp_name']); 
+        $a->decodePDF();
+        $text = $a->output(); 
+		echo nl2br($text);
+
+		$horaFin = microtime(true);
+		$timeReading = round($horaFin - $horaInicio, 3);
+		error_log('['.date('Y-m-d H:i:s').'] TXTReader / IP: '.$_SERVER['REMOTE_ADDR'].' | Response: '.$timeReading.PHP_EOL, 3, 'logs.txt');
+    }
+     
+    else {
+        echo "<p style='color:red; text-align:center'>
+            Wrong file format</p>";
+    }
+}    
+?>
+
+    <form method="post" enctype="multipart/form-data">
+        Choose Your File
+        <input type="file" name="file" />
+        <br>
+        <input type="submit" value="Read PDF" name="readpdf" />
+    </form>
+</body>
+
+</html>
---
+++ txtReader3.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="por">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>txtReader</title>
+</head>
+<body>
+<?php
+ 
+require_once 'vendor/autoload.php';
+
+extract($_POST);
+
+if(isset($readpdf)){
+    
+    if($_FILES['file']['type']=="application/pdf") {
+		$horaInicio = microtime(true);
+
+        $parser = new \Smalot\PdfParser\Parser();
+        $pdf = $parser->parseFile($_FILES['file']['tmp_name']);
+
+        $text = $pdf->getText();
+        echo nl2br($text);
+
+        $horaFin = microtime(true);
+		$timeReading = round($horaFin - $horaInicio, 3);
+		error_log('['.date('Y-m-d H:i:s').'] TXTReader3 / IP: '.$_SERVER['REMOTE_ADDR'].' | Response: '.$timeReading.PHP_EOL, 3, 'logs.txt');
+    }
+     
+    else {
+        echo "<p style='color:red; text-align:center'>
+            Wrong file format</p>";
+    }
+}    
+?>
+
+    <form method="post" enctype="multipart/form-data">
+        Choose Your File
+        <input type="file" name="file" />
+        <br>
+        <input type="submit" value="Read PDF" name="readpdf" />
+    </form>
+</body>
+
+</html>
